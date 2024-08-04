aa390c5cdcddabd821e7bd64796a398bf92f9269
Sveta Baksichava <svetabaksichava@Alexeis-MacBook-Air.local>
2024-08-04 16:47:08 +0300

    feat: add reverse int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+    let str = n.toString();  
+    let reversedStr = str.split('').reverse().join('');
+    let reversedNum = parseInt(reversedStr, 10);  
+    return reversedNum;
+  }
   
-}
